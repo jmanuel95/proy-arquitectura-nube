76872e47d3d920bcc007daf57032a1c12cabd025
juan- <Estefania.torrez@udea.edu.co>
2025-10-01 21:07:42 -0500

    feat(arquitectura): se despliega segundo microservicio

--- lambda/add_event/app.py
+++ lambda/add_event/app.py
@@ -19,28 +19,29 @@ def handler(event, context):
         if "Item" not in user_resp:
             return {"statusCode": 404, "body": json.dumps({"error": "Usuario no encontrado"})}
 
-        role = user_resp["Item"].get("Role")
-        if role == "CLIENTE":
-            return {"statusCode": 403, "body": json.dumps({"error": "Clientes no pueden crear eventos"})}
+        role = user_resp["Item"].get("role")
+
+        # Insrtar evento
+        if role == "ADMIN":
+            event_item = {
+                "EventId": body["EventId"],
+                "EventName": body["EventName"],
+                "EventDate": body["EventDate"],
+                "EventStatus": body["EventStatus"],
+                "EventCountry": body["EventCountry"],
+                "EventCity": body["EventCity"],
+                "UserId": body["UserId"],
+                "Quantity": body["Quantity"],
+                }
+            events_table.put_item(Item=event_item)
+
+            return {
+                "statusCode": 201,
+                "body": json.dumps({"message": "Evento creado exitosamente", "event": event_item})
+            }
         
-
-        # Insertar evento
-        event_item = {
-            "EventId": body["EventId"],
-            "EventName": body["EventName"],
-            "EventDate": body["EventDate"],
-            "EventStatus": body["EventStatus"],
-            "EventCountry": body["EventCountry"],
-            "EventCity": body["EventCity"],
-            "UserId": body["UserId"],
-            "Quantity": body["Quantity"],
-        }
-        events_table.put_item(Item=event_item)
-
-        return {
-            "statusCode": 201,
-            "body": json.dumps({"message": "Evento creado exitosamente", "event": event_item})
-        }
+        else:
+            return {"statusCode": 403, "body": json.dumps({"error": "Clientes no pueden crear eventos"})}
 
     except Exception as e:
         return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
